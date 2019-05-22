1620c4feaad96e6d081fafdee0833162a2b47538
mac <xuandong@juxiangfen.com>
2019-05-22 22:01:28 +0800

    1、BaseActivity 权限请求

--- basemodule/src/main/java/me/xdd/self/basemodule/base/BaseActivity.java
+++ basemodule/src/main/java/me/xdd/self/basemodule/base/BaseActivity.java
@@ -1,8 +1,11 @@
 package me.xdd.self.basemodule.base;
 
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
@@ -22,6 +25,7 @@ import xiaoqiao.qiaorong.com.uimodule.toast.Toasty;
  * @author xuandong on 2019/5/15
  */
 public abstract class BaseActivity extends AppCompatActivity implements OnGlideDisplayInterface,Init,JsonCallback.HandleResponse {
+    private static final int PERMISSION_REQUEST_CODE = 10000;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -187,5 +191,69 @@ public abstract class BaseActivity extends AppCompatActivity implements OnGlideD
     public <T> void onSuccess(Response<T> response) {
 
     }
+
+    protected void requestPermission(String[] permissions){
+        boolean isAllGranted = true;
+        for (int i = 0;i<permissions.length;i++){
+            if (ActivityCompat.checkSelfPermission(this,permissions[i]) != PackageManager.PERMISSION_GRANTED){
+                isAllGranted = false;
+                break;
+            }
+        }
+
+        if (isAllGranted){
+            permissionAllGranted();
+        }else{
+            ActivityCompat.requestPermissions(this,permissions,PERMISSION_REQUEST_CODE);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE){
+            boolean isAllGranted = true;
+            int position = 0;
+            for (int i = 0;i<grantResults.length;i++){
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                    isAllGranted = false;
+                    position = i;
+                    break;
+                }
+            }
+
+            if (isAllGranted){
+                permissionAllGranted();
+            }else{
+                if (!ActivityCompat.shouldShowRequestPermissionRationale(this,permissions[position])){
+                    permissionRationale(permissions[position],position);
+                }else{
+                    permissionRefuse(permissions[position],position);
+                }
+            }
+        }
+    }
+
+    /**
+     * 权限申请成功
+     */
+    protected void permissionAllGranted(){
+
+    }
+
+    /**
+     * 权限申请拒绝并且不再询问
+     * @param permission
+     * @param position
+     */
+    protected void permissionRationale(String permission,int position){
+
+    }
+
+    protected void permissionRefuse(String permission,int position){
+
+    }
+
+
 
 }
